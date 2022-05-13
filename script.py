623aee528722e63d52a08ca9a493290521a99040
Borzhikov Temirlan <pitshow277@gmail.com>
2022-05-14 00:55:20 +0700

    Кнопка
    Была добавлена функциональность нажатия кнопки

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
 import time
 from selenium import webdriver
+
 browser = webdriver.Chrome()
 browser.get("https://coub.com/")
 
 input1 = browser.find_element_by_id("q")
 input1.send_keys("коты")
+
+
+button = browser.find_element_by_css_selector("button.sb")
+button.click()
+
+
